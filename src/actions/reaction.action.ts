72bd29f274737b153fa46c38a07194967aea623f
Иванин Александр <ivanin.aleksandr5@wb.ru>
2025-08-07 21:00:29 +0300

    feat(reaction-action): added new action for bot

---
+++ src/actions/reaction.action.ts
@@ -0,0 +1,58 @@
+// src/actions/RandomReactionsAction.ts
+import { injectable, inject } from 'inversify';
+import { message } from 'telegraf/filters';
+import { BotService } from '../services/bot.service';
+import { ReactionType } from 'telegraf/types';
+import { ActionInterface } from '../interfaces/action.interface';
+import { LoggerService } from '../services/logger.service';
+import { LoggerInterface } from '../interfaces/logger.interface';
+
+@injectable()
+export class RandomReactionsAction implements ActionInterface {
+  private readonly probability = 0.1;
+  private readonly emojiPool: ReactionType[] = [
+    { emoji: '👍', type: 'emoji' },
+    { emoji: '🔥', type: 'emoji' },
+    { emoji: '😁', type: 'emoji' },
+    { emoji: '😎', type: 'emoji' },
+    { emoji: '❤‍🔥', type: 'emoji' },
+    { emoji: '💩', type: 'emoji' },
+    { emoji: '⚡', type: 'emoji' },
+    { emoji: '👀', type: 'emoji' },
+  ];
+
+  constructor(
+    @inject(BotService) private botSrv: BotService,
+    @inject(LoggerService) private loggerSrv: LoggerInterface
+  ) {}
+
+  public register(): void {
+    this.botSrv.bot.on(message('text'), async (ctx, next) => {
+      if (ctx.message.text.startsWith('/')) return await next();
+
+      if (Math.random() > this.probability) return await next();
+
+      const randomEmoji =
+        this.emojiPool[Math.floor(Math.random() * this.emojiPool.length)];
+
+      try {
+        await ctx.telegram.setMessageReaction(
+          ctx.chat.id,
+          ctx.message.message_id,
+          [randomEmoji],
+          false
+        );
+
+        this.loggerSrv.info(
+          `🎯 Поставлена реакция ${randomEmoji} на сообщение: ${ctx.message.text}`
+        );
+      } catch (error) {
+        if (typeof error === 'string') {
+          this.loggerSrv.error(error);
+        } else if (error instanceof Error) {
+          this.loggerSrv.error(error.message);
+        }
+      }
+    });
+  }
+}
