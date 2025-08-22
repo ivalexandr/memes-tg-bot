8b6f172f0d442de612e8d9b0ec2f70f8e6c7691f
Иванин Александр <ivanin.aleksandr5@wb.ru>
2025-08-23 00:52:01 +0300

    fix(ai-conversation): key without thread key

--- src/utils/make-context-key.util.ts
+++ src/utils/make-context-key.util.ts
@@ -3,7 +3,5 @@ import { Context } from 'telegraf';
 export const makeContextKey = (ctx: Context): string | null => {
   const chatId = ctx.chat?.id;
   if (!chatId) return null;
-  const isSupergroup = (ctx.chat.type = 'supergroup');
-  const threadId = isSupergroup ? ctx.message?.message_thread_id : 0;
-  return `${chatId}::${threadId}`;
+  return `${chatId}`;
 };
